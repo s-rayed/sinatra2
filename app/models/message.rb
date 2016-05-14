d9553c7e1b5bd4e9b0727439fcaf3465ad54d7ce
Rayed <siddiqui.rayed@gmail.com>
2016-05-14 18:04:00 -0400

    added validations to posts

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ActiveRecord::Base
 
+  validates :title, presence: true, length: {minimum: 2, maximum: 140}
+  validates :author, presence: true, length: {minimum: 1, maximum: 25}
+
 end
